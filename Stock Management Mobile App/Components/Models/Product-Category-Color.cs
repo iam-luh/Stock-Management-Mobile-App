096c63cdbdfd63afa9339e2bff35202c70aef82f
iam-luh <lusungugodwin20@gmail.com>
2024-10-02 22:05:43 +0300

    Normalization of the Database

---
+++ Stock Management Mobile App/Components/Models/Product-Category-Color.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Management_Mobile_App.Components.Models
+{
+    public class Product_Category_Color
+    {
+        public int ProductId { get; set; }
+        public string? Category {  get; set; }
+        public string? Color { get; set; }
+    }
+}
